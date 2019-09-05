e49737763e0fc4d32554bd8fc40efefdcd03cd75
miju462.park <miju462.park@hanwha.com>
2019-09-05 10:06:42 +0900

    Verification Entity 추가

---
+++ src/entities/Verification.ts
@@ -0,0 +1,31 @@
+import { verificationTarget } from "src/types/types";
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
+} from "typeorm";
+
+@Entity()
+class Verification extends BaseEntity {
+  @PrimaryGeneratedColumn() id: number;
+
+  @Column({ type: "text", enum: ["EMAIL", "PHONE"] })
+  target: verificationTarget;
+
+  @Column({ type: "text" })
+  payload: string;
+
+  @Column({ type: "text" })
+  key: string;
+
+  @Column({ type: "boolean" })
+  used: boolean;
+
+  @CreateDateColumn() createAt: string;
+  @UpdateDateColumn() updateAt: string;
+}
+
+export default Verification;
